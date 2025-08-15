e9076cc3ff30f2c0fcab797a428afa2aeb4098e1
aneetan <anitaneupane2061@gmail.com>
2025-08-15 10:58:23 +0545

    Dockerizing react app with project configuration

---
+++ AidLink/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='font-semibold text-red-600'> Hello from React + TS with Tailwind</h1>
+    </>
+  )
+}
+
+export default App
