ce2a4850d4acaf05419a0255ec56d11365eb5b92
aneetan <anitaneupane2061@gmail.com>
2025-08-15 11:54:06 +0545

    test: Ci/cd updates workflow

--- AidLink/src/App.tsx
+++ AidLink/src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [gate, setGate] = useState();
 
   return (
     <>
       <h1 className='font-semibold text-green-600'> Hello from React + TS with Tailwind</h1>
+      <p className='text-green-400'> This is green </p>
     </>
   )
 }
